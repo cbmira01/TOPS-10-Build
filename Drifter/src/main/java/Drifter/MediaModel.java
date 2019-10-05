dfaacf34ac57bdabd3b67ee9e8d2d8adfc89a5ca
Calvin Miracle <cbmira01@gmail.com>
2019-10-05 14:14:51 -0400

    Add stubs for media and tape models

---
+++ Drifter/src/main/java/Drifter/MediaModel.java
@@ -0,0 +1,13 @@
+
+package Drifter;
+
+/**
+ *
+ * @author Calvin
+ */
+public class MediaModel {
+    // TODO: this could be a contract (interface) implemented by all media types
+    
+    // media type, eg tape, disk, for use in SIMH
+
+}
---
+++ Drifter/src/main/java/Drifter/TapeModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Drifter;
+
+/**
+ *
+ * @author Calvin
+ */
+public class TapeModel {
+    // TODO: tape properties
+    
+    // tape name, string
+    // tape label, string
+    // purpose, string
+    // unpacked local filename, string
+    //
+    // list of URLs and associated attributes
+}
